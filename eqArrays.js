cad1e03edfe4db4d7022dea18c5d6e4a9eeaa700
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-18 23:33:25 +0000

    function for comparing array values

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ? console.log(`Assertion Passed👍: ${actual} === ${expected}`) : console.log(`Assertion Failed👎: ${actual} !== ${expected}`);
+};
+
+//assertEqual("wroooong", "Bootcamp");
+//assertEqual(1, 1);
+
+const eqArrays = (arr1, arr2) => {
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2[i] !== arr1[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS)
