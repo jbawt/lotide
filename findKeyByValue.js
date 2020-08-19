4ebfae9b5121161c41efa514f85e19bd26f5396b
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-19 22:08:17 +0000

    find the key by the value function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ? console.log(`Assertion Passed👍: ${actual} === ${expected}`) : console.log(`Assertion Failed👎: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, val) => {
+  let result;
+
+  for (const key in obj) {
+    if (obj[key] === val) {
+      result = key;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
