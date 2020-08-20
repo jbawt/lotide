522505d80f0a89d9e72fc6d4fca9ba007e0031b6
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-20 21:41:07 +0000

    higher order find key function with call back

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ? console.log(`Assertion Passed👍: ${actual} === ${expected}`) : console.log(`Assertion Failed👎: ${actual} !== ${expected}`);
+};
+
+const findKey = (obj, callBack) => {
+  let result;
+
+  for (let key in obj) {
+    if (callBack(obj[key])) {
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+
+let test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+assertEqual(test1, 'noma'); // => pass
+assertEqual(test1, 'Ora'); // => fail
+
+let test2 = findKey({
+  hello: { num: 1 },
+  how: { num: 2 },
+  are: { num: 3 },
+  you: { num: 3 }
+}, b => b.num === 3); // => are
+assertEqual(test2, 'are'); // => pass
+assertEqual(test2, 'you'); // => fail
